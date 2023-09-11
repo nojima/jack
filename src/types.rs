60b5e5fa142abcfc8ab469529919c1f7cda1d41d
Yusuke Nojima <nojima@ynojima.com>
2023-09-11 23:23:16 +0900

    Add `types::Type`

--- src/types.rs
+++ src/types.rs
@@ -1,5 +1,43 @@
 use std::fmt::{self, Display, Formatter};
 
+#[allow(dead_code)]
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum Type {
+    Null,
+    Bool,
+    Number,
+    String,
+    Array(Box<Type>),
+    Dict(Box<Type>),
+    Function(Vec<Type>, Box<Type>),
+}
+
+impl Display for Type {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match self {
+            Type::Null => write!(f, "Null"),
+            Type::Bool => write!(f, "Bool"),
+            Type::Number => write!(f, "Number"),
+            Type::String => write!(f, "String"),
+            Type::Array(t) => write!(f, "Array[{t}]"),
+            Type::Dict(t) => write!(f, "Dict[{t}]"),
+            Type::Function(params, ret) => {
+                write!(f, "(")?;
+                let mut first = true;
+                for t in params {
+                    if first {
+                        first = false;
+                    } else {
+                        write!(f, ", ")?;
+                    }
+                    t.fmt(f)?;
+                }
+                write!(f, ") => {ret}")
+            }
+        }
+    }
+}
+
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Erasure {
     Null,
